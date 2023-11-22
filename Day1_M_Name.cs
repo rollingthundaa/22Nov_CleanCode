685784b2f5c5648e5985eac31d0ab29ba2b284ac
Devadyuti Sahoo <59117554+rollingthundaa@users.noreply.github.com>
2023-11-22 11:11:20 +0530

    Create Day1_M_Name.cs

---
+++ Day1_M_Name.cs
@@ -0,0 +1,57 @@
+review1.cs
+int d; // elapsed time in days
+int elapsedDays;
+
+review2.cs
+var dataFromDb = db.GetFromService().ToList(); // Get List of employees 
+var employeeDetails = employeeDb.GetFromEmployeeService().GetEmployeesList();
+
+review3.cs
+int iCounter;
+string strFullName;
+DateTime dModifiedDate;
+int counter;
+string fullName;
+DateTime modifiedDate;
+
+review4.cs
+public bool IsShopOpen(string pDay, int pAmount)
+{
+    // some logic
+}
+public bool IsShopOpen(string Day, int Amount)
+{
+    // some logic
+}
+
+review5.cs
+const int DAYS_IN_WEEK = 7;
+const int daysInMonth = 30;
+const int daysInWeek = 7;
+const int daysInMonth = 30;
+
+review6.cs
+public class Employee
+{
+    public Datetime sWorkDate { get; set; } // get set Start Working Date
+    public Datetime modTime { get; set; } // get set Modification Time
+}
+public class Employee
+{
+    public Datetime startWorkingDate { get; set; } // get set Start Working Date
+    public Datetime modificationTime { get; set; } // get set Modification Time
+}
+
+review7.cs
+var employeephone;
+
+public double CalculateSalary(int workingdays, int workinghours)
+{
+    // some logic
+}
+var employeephone;
+
+public double calculateSalary(int workingDays, int workingHours)
+{
+    // some logic
+}
