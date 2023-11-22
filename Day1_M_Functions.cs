1d2b312e58bcf170c53c7cac2b713199132ee9e2
Devadyuti Sahoo <59117554+rollingthundaa@users.noreply.github.com>
2023-11-22 13:47:35 +0530

    Create Day1_M_Functions.cs

---
+++ Day1_M_Functions.cs
@@ -0,0 +1,80 @@
+review1.cs
+package CS1;
+public class UserValidator 
+{
+    private Cryptographer cryptographer;
+    public boolean checkPasswordAndInitializeSession(String userName, String password) 
+    {
+        User user = UserGateway.findByName(userName);
+        if (user != null) 
+        {
+            String codedPhrase = user.getPhraseEncodedByPassword();
+            String phrase = cryptographer.decrypt(codedPhrase, password);
+            if ("Valid Password".equals(phrase)) 
+            {
+                session.initialize();
+                return true;
+            }
+        }      
+        return false;
+    }
+}
+
+review2.cs
+var name = "Ryan McDermott";
+var splitted_name = name.Split(" ");
+name = $"His first name is {splitted_name[0]}, and his last name is {splitted_name[1]}"; 
+Console.WriteLine(name);
+
+review3.cs
+public bool IsDOMNodePresent(string node)
+{
+    // ...
+}
+
+if (!IsDOMNodePresent(node))
+{
+    // ...
+}
+
+review4.cs
+abstract class Airplane
+{
+    public abstract double GetCruisingAltitude();
+}
+ 
+class 777 : Airplane {
+    public override double GetCruisingAltitude()
+    {
+        return GetMaxAltitude() - GetPassengerCount();
+    }
+}
+
+class AirForceOne : Airplane {
+    public override double GetCruisingAltitude()
+    {
+        return GetMaxAltitude();
+    }
+}
+
+class Cessna :Airplane {
+    public override double GetCruisingAltitude()
+    {
+        return GetMaxAltitude() - GetFuelExpenditure();
+    }
+}
+
+review6.cs
+public void CreateFile(string name)
+{
+   
+        Touch(name);
+    
+}
+public void CreateTempFile(string name)
+{
+  Touch("./temp/" + name);
+}
+
+
+
